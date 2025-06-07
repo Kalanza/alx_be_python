b941f9d789a655aa421f25060cd6c4f6f251508f
Kalanza <kalanzavictor@gmail.com>
2025-06-07 07:00:26 +0300

    fixed the bug in exploredatetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,15 +1,15 @@
-import datetime
+from datetime import datetime, date, timedelta
 #This two enable us to workk with dates,time and yeard
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     print(current_date)
 
 display_current_datetime()
 
 def calculate_future_date():
     days = int(input('Enter the number days you want to add: '))
-    today = datetime.date.today()
-    future_date = today + datetime.timedelta(days=days)
+    today = date.today()
+    future_date = today + timedelta(days=days)
   
     print("Future date:", future_date.strftime("%Y-%m-%d"))
 
