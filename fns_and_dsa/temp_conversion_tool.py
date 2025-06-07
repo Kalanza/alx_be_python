675c8a34b49ed2ef0143380db1531ff74cf32a81
Kalanza <kalanzavictor@gmail.com>
2025-06-07 07:35:33 +0300

    fixed the bug

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,15 +3,17 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
     temp_celsius = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
-    print(temp_celsius)
+    return temp_celsius
 
 def convert_to_fahrenheit(celsius):
     temp_fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
-    print(temp_fahrenheit) 
+    return temp_fahrenheit 
 
 x = float(input('Enter the temperature to convert: '))
 y = input('Is this temperature in Celsius or Fahrenheit? (C/F) ')
 if y == 'C':
-    convert_to_fahrenheit(x)
+    a = convert_to_fahrenheit(x)
+    print(a,'°C is',x,'°F')
 else:
-    convert_to_celsius(x)
+    a = convert_to_celsius(x)
+    print(x,'°F is',a,'°C')
