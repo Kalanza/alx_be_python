f214b39d5552bfbde618eb6863d49ddb49273cb0
Kalanza <kalanzavictor@gmail.com>
2025-06-15 17:36:44 +0300

    Refactor BankAccount methods for consistency and clarity

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self, account_balance = 0):
+    def __init__(self, account_balance=0):
         self.account_balance = account_balance
 
     def deposit(self, amount):
@@ -7,12 +7,12 @@ class BankAccount:
         return self.account_balance
 
     def withdraw(self, amount):
-        if amount < self.account_balance:
-           self.account_balance -= amount
-           return self.account_balance
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
         else:
-            self.account_balance
-    
+            return False
+
     def display_balance(self):
-        print(f"$Current Balance: {self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
 
