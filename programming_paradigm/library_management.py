46d3327dbbba6f37e1014ba9a35b5f4d7b9eb62f
Kalanza <kalanzavictor@gmail.com>
2025-06-17 08:26:31 +0300

    Refactor Book and Library classes for improved readability and consistency

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__ (self,title,author):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
         self.is_checked_out = False
@@ -14,24 +14,25 @@ class Book:
         self.is_checked_out = False
 
 class Library:
-    def __init__(self, books):
+    def __init__(self):
         self.books = []
     
-    def add_book(self,book):
-        return self.books.append(book)
+    def add_book(self, book):
+        self.books.append(book)
     
-    def check_out_book(self,title):
-        for book  in self._books:
+    def check_out_book(self, title):
+        for book in self.books:
             if book.title == title and not book.is_checked_out():
                 book.check_out()
                 return True
         return False
     
-    def return_book(self,title):
-        for book in self._books:
+    def return_book(self, title):
+        for book in self.books:
             if book.title == title and book.is_checked_out():
                 book.return_book()
                 return True
         return False
-    def list_available_books(self,book):
-        return [book for book in self._books if not book.is_checked_out()]
+    
+    def list_available_books(self):
+        return [book for book in self.books if not book.is_checked_out()]
